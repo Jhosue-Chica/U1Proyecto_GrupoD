f291702bdc4e7610e4006b392f2fbb64ff0f4f6a
raulfaz <refaz@espe.edu.ec>
2024-06-10 20:32:56 -0500

    footer 1.0

--- scripts/Footer.js
+++ scripts/Footer.js
@@ -11,18 +11,43 @@ class MyFooter extends HTMLElement {
     render() {
         //const title = this.getAttribute('title');
         const frase = this.getAttribute('frase');
-      //  const customAttribute = this.getAttribute('custom-attribute');
+        //  const customAttribute = this.getAttribute('custom-attribute');
         this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="../styles.css">
+            <link rel="stylesheet" href="/styles/footer.css">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
             <div class="footer">
-            <div class="row mt-4 p-4">
-            <div class="col-md-5 col-12 logo-footer"><img src="../img/2.png"></div>
-            <div class="col-md-5 col-12 frase">${frase}</div>
+                <div class="row mt-4">
+                    <div class="col-md-4 col-12 logo-footer "><img src="../img/2_copia.png"></div>
+                    <div class="col-md-6 col-12  mt-4 frase"><strong>${frase}</strong></div>
+                    <hr style="border-top: 2px solid #ffffff; width:70%; border-radius:2px;">
+                </div>
+                <div class="row mt-4 relleno_footer">
+                    <div class="col-md-4 col-12 col-lg-4 text-center redes_sociales">
+                     <ul style="list-style-type: none;">
+                        <li><strong>Redes Sociales</strong></li>
+                        <li><img  src="/img/icon.png"><img  src="/img/icon1.png"><img  src="/img/icon2.png"></li>
+                         <li>Item 3</li>
+                    </ul>
+                </div>
+                <div class="col-md-4 col-12 col-lg-4 text-center">
+            <ul style="list-style-type: none;">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            </ul>
             </div>
-            <hr style="border-top: 2px solid #ffffff; width:70%; border-radius:2px;">
+            <div class="col-md-4 col-12 col-lg-4 text-center">
+            <ul style="list-style-type: none;">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            </ul>
+            </div>
+            </div>
             <br>
-            <slot></slot>
+            <div class="row mt-4 text-center bg-ligth">
+            <p> 2024 ECO ESPE. Todos los derechos reservados.</p>
+            </div>
             </div>
         `;
     }
